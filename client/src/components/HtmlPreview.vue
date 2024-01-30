74e44234578a22c7feec415161bd849ebdaff21e
rob <58299468+b7c@users.noreply.github.com>
2024-01-30 23:43:53 +1300

    remove unnecessary imports

--- client/src/components/HtmlPreview.vue
+++ client/src/components/HtmlPreview.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, defineEmits, defineProps, watch } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 
 const frameRef = ref<HTMLIFrameElement>()
 const props = defineProps<{ content: string | undefined }>()
