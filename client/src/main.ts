03fd7c45061fba7c7b4246f47a320170c4191752
rob <58299468+b7c@users.noreply.github.com>
2024-03-04 18:23:53 +1300

    add fontawesome icons

--- client/src/main.ts
+++ client/src/main.ts
@@ -7,7 +7,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faEdit)
+
 const app = createApp(App)
+
+app.component('fa-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
